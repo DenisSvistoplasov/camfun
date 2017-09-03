6ebcbe5e02058175178a244f46ebf98e70a5f58e
Anton Iun <nah7c00@gmail.com>
2017-09-04 04:06:42 +0900

    Update

--- remote.sh
+++ remote.sh
@@ -4,6 +4,10 @@ docker save -o camfun2.tar camfun2
 gzip -c camfun2.tar > camfun2.tar.gz
 rm camfun2.tar
 scp camfun2.tar.gz asus:~/
+ssh asus "docker stop camfun2"
+ssh asus "docker rm camfun2"
+ssh asus "docker rmi camfun2"
 ssh asus "docker load -i ~/camfun2.tar.gz"
+ssh asus "docker run --detach --name=camfun2 --publish=32780:80 --restart=always camfun2"
 rm camfun2.tar.gz
 
