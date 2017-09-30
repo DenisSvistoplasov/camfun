5508987804fa053508138f3113e7d0e9404d1602
Anton Iun <iunanton@naver.com>
2017-10-01 01:10:58 +0800

    Change default port

--- do.sh
+++ do.sh
@@ -3,7 +3,7 @@
 docker build -t camfun2 .
 docker run --detach \
 	--name=camfun2 \
-	--publish-all \
+	--publish=32769:80 \
 	--restart=always \
 	camfun2 > /dev/null
 docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' camfun2
